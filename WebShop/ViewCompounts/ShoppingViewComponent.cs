01f4624e6de0238fd2ee3ef68af52e0522125692
MohamedGamal Idress <Mohamed_Gamal_22@outlook.com>
2024-07-25 15:59:34 +0300

    UI Modify1

--- WebShop/ViewCompounts/ShoppingViewComponent.cs
+++ WebShop/ViewCompounts/ShoppingViewComponent.cs
@@ -2,15 +2,14 @@
 using myShop.Entity.Repository;
 using myShop.Uititlity;
 using System.Security.Claims;
-namespace myShop.Web.ViewCompount.ShoppingCart
- 
+namespace myShop.Web.ViewCompounts
+
 {
-    
-    public class ShoppingCartViewComponent : ViewComponent
+    public class ShoppingViewComponent : ViewComponent
     {
         private readonly IUnitOfWork unitOfWork;
 
-        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        public ShoppingViewComponent(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork;
         }
@@ -20,9 +19,9 @@ namespace myShop.Web.ViewCompount.ShoppingCart
             var clamidentity = (ClaimsIdentity)User.Identity;
             var user = clamidentity.FindFirst(ClaimTypes.NameIdentifier);
 
-            if(user != null)
+            if (user != null)
             {
-                if(HttpContext.Session.GetInt32(SD.SessionKey)!= null)
+                if (HttpContext.Session.GetInt32(SD.SessionKey) != null)
                 {
                     return View(HttpContext.Session.GetInt32(SD.SessionKey));
                 }
@@ -30,7 +29,7 @@ namespace myShop.Web.ViewCompount.ShoppingCart
                 {
                     var val = unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUsersId == user.Value).Count();
                     HttpContext.Session.SetInt32(SD.SessionKey, val);
-                   return View(HttpContext.Session.GetInt32(SD.SessionKey));
+                    return View(HttpContext.Session.GetInt32(SD.SessionKey));
                 }
             }
             else
